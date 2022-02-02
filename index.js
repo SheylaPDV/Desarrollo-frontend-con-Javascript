8ba9274cc96ea373310036898334e9265acace86
Sheyla Perez <sheylapdv@gmail.com>
2022-02-02 20:08:22 +0100

    index

--- index.js
+++ index.js
@@ -1,23 +1,47 @@
 
-const tweet = {
-    handler: 'user01',
+const tweets = [
+    {
+    handler: 'Patinete',
     content: 'Algo que publico',
-    created_at: '2022-01-31',
-};
+    created_at: '2022-01-31'
+    },
+    {
+    handler: 'user02',
+    content: 'Algo que publico',
+    created_at: '2022-01-10',
+    },
+    {
+    handler: 'user03',
+    content: 'Algo que publico',
+    created_at: '2022-01-29'
+    },
+];
 
 // contenedor donde pintar los tweets
 const tweetListElement = document.querySelector('.tweet-list');
 
-// generar HTML del tweet
-const currentTime = new Date(tweet.created_at)
-const tweetTemplate = `
-    <h1>${tweet.handler}</h1>
-    <p>${tweet.content}</p>
-    <p>${currentTime}</p>
-`;
 
-const tweetArticleElement = document.createElement('article');
-tweetArticleElement.innerHTML = tweetTemplate;
-// incluir tweet en el DOM
-tweetListElement.appendChild(tweetArticleElement);
+
+// generar HTML del tweet
+
+
+for (const tweet of tweets) {
+    const currentTime = new Date(tweet.created_at).toISOString();
+
+    const tweetTemplate = `
+        <h1>${tweet.handler}</h1>
+        <p>${tweet.content}</p>
+        <p>${currentTime}</p>
+    `;
+
+    const tweetArticleElement = document.createElement('article');
+    tweetArticleElement.innerHTML = tweetTemplate;
+
+    // incluir tweet en el DOM
+    tweetListElement.appendChild(tweetArticleElement);
+};
+
+
+
+
 
