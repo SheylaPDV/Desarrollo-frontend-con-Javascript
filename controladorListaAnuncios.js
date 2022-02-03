9ab5aa3ab3cfc3bea1fd397182b4528746d1a272
Sheyla Perez <sheylapdv@gmail.com>
2022-02-03 20:46:14 +0100

    mas cambios

--- controladorListaAnuncios.js
+++ controladorListaAnuncios.js
@@ -3,21 +3,17 @@ import modeloServicioWallapop from "./modeloServicioWallapop.js";
 import { constructorAnuncios } from "./vistaAnuncios.js";
 
 
-export function controladorListaAnuncios() {
-    // contenedor donde pintar los tweets
-    const listaDeAnuncios = document.querySelector('.lista-anuncios');
-
+export function controladorListaAnuncios(listaDeAnuncios) {
+    // contenedor donde pintar los anuncios
     const anuncios = modeloServicioWallapop.getAnuncios();
 
-    
-
-    // generar HTML del tweet
+    // generar HTML del anuncio
     for (const anuncio of anuncios) {
         const elementoDeArticulo = document.createElement('article');
         const anuncioTemplate = constructorAnuncios(anuncio);
 
         elementoDeArticulo.innerHTML = anuncioTemplate;
-        // incluir tweet en el DOM
+        // incluir anuncio en el DOM
         listaDeAnuncios.appendChild(elementoDeArticulo);
     };
 }
--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 import { controladorListaAnuncios } from "./controladorListaAnuncios.js";
 
-controladorListaAnuncios();
+document.addEventListener('DOMContentLoaded', () => {
+    const listaDeAnuncios = document.querySelector('.lista-anuncios');
+    controladorListaAnuncios(listaDeAnuncios);
+});
+
+// cuando mi DOM este listo, ejecuto el controlador
