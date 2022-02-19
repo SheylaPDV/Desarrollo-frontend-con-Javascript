90a0aeda3d5bf989570927aa55e56b14693fd0c8
Sheyla Perez <sheylapdv@gmail.com>
2022-02-19 14:32:21 +0100

    creo login

--- login/controladorLogin.js
+++ login/controladorLogin.js
@@ -1,6 +1,57 @@
+import { servicioCrearCuenta } from "../crearCuenta/servicioCrearCuenta.js";
+import { pubSub } from "../pubSub.js";
+
 export class ControladorLogin {
     constructor(elementoLogin) {
         this.elementoLogin = elementoLogin;
+
+        this.adjuntarEventos()
+    }
+
+    adjuntarEventos() {
+        this.cambioDeInput();
+        this.enviarFormularioLogin();
+    }
+
+    cambioDeInput() {
+        const elementosInputs = Array.from(this.elementoLogin.querySelectorAll('input'));
+
+        elementosInputs.forEach(elementoInput => {
+            elementoInput.addEventListener('input', () => {
+               const inputsLlenos = elementosInputs.every(elementoInput => elementoInput.value);
+
+               if (inputsLlenos) {
+                   this.elementoLogin.querySelector('button').removeAttribute('disabled');
+               } else {
+                    this.elementoLogin.querySelector('button').setAttribute('disabled', "");
+               }
+            });
+        });
+    }
+
+    enviarFormularioLogin() {
+        this.elementoLogin.addEventListener('submit', (event) => {
+            event.preventDefault();
+
+            const formData = new FormData(this.elementoLogin);
+
+            const username = formData.get('user');
+            const password = formData.get('password');
+
+            this.loginUsuario(username, password);
+        });
+    }
+
+    async loginUsuario(username, password) {
+        try {
+            await servicioCrearCuenta.loginUsuario(username, password);
+            window.location.href = '/';
+          
+        } catch (error) {
+            pubSub.publish(pubSub.TOPICS.SHOW_ERROR_NOTIFICATION, error);
+        }
+
     }
 }
 
+ 
--- login/login.js
+++ login/login.js
@@ -1,7 +1,12 @@
-import { ControladorLogin } from "./controladorLogin";
+import { ControladorLogin } from "./controladorLogin.js";
+import { ControladorNotificaciones } from "../notificaciones/controladorNotificaciones.js";
 
-document.addEventListener('DomContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
     const loginFormulario = document.querySelector('form');
+    const elementoNotificacion = document.querySelector('.notificacion');
+
+    const controladorNotificaciones = new ControladorNotificaciones(elementoNotificacion);
+    
 
     const controladorLogin = new ControladorLogin(loginFormulario);
 })
